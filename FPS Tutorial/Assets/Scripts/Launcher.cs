2a55d755726c5ed05eeb488402f4c5e9eedb7215
Ido Rosenberger <idoldo217@gmail.com>
2021-12-27 16:36:49 +0200

    added room browser

--- FPS Tutorial/Assets/Scripts/Launcher.cs
+++ FPS Tutorial/Assets/Scripts/Launcher.cs
@@ -27,6 +27,10 @@ public class Launcher : MonoBehaviourPunCallbacks
     public GameObject errorScreen;
     public TMP_Text errorText;
 
+    public GameObject roomBrowserScreen;
+    public RoomButton theRoomButton;
+    private List<RoomButton> allRoomButtons = new List<RoomButton>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,6 +48,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         createRoomScreen.SetActive(false);
         roomScreen.SetActive(false);
         errorScreen.SetActive(false);
+        roomBrowserScreen.SetActive(false);
     }
 
     public override void OnConnectedToMaster()
@@ -72,10 +77,6 @@ public class Launcher : MonoBehaviourPunCallbacks
             CloseMenus();
             loadingText.text = "Creating Room...";
             loadingScreen.SetActive(true);
-        }
-        else
-        {
-            
         }
     }
 
@@ -108,5 +109,47 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         CloseMenus();
         menuButtons.SetActive(true);
+    }
+    public void OpenRoomBrowser()
+    {
+        CloseMenus();
+        roomBrowserScreen.SetActive(true);
+    }
+    public void CloseRoomBrowser()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(RoomButton rb in allRoomButtons)
+        {
+            Destroy(rb.gameObject);
+        }
+        allRoomButtons.Clear();
+        theRoomButton.gameObject.SetActive(false);
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            if(roomList[i].PlayerCount != roomList[i].MaxPlayers && !roomList[i].RemovedFromList)
+            {
+                RoomButton newButton = Instantiate(theRoomButton, theRoomButton.transform.parent);
+                newButton.SetButtonDetails(roomList[i]);
+                newButton.gameObject.SetActive(true);
+
+                allRoomButtons.Add(newButton);
+            }
+        }
+    }
+    public void JoinRoom(RoomInfo inputInfo)
+    {
+        PhotonNetwork.JoinRoom(inputInfo.Name);
+
+        CloseMenus();
+        loadingText.text = "Joining Room...";
+        loadingScreen.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
